c941333cd1c1db6680a2d965eda6b80ae63de767
jacobHildebrandt <jake@hildy.ca>
2020-07-19 16:10:19 -0700

    Added the mobile controls

---
+++ Mobile_FPS/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public Joystick joystick;
+
+    private RigidbodyFirstPersonController rigidBodyController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() 
+    {
+        rigidBodyController.joystickInputAxis.x = joystick.Horizontal;
+        rigidBodyController.joystickInputAxis.y = joystick.Vertical;
+    }
+}
