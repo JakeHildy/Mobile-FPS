2ce2ca8fee5a69123da1391a600d1fa430a49994
jacobHildebrandt <jake@hildy.ca>
2020-07-20 21:50:05 -0700

    Added walk & run blend trees

--- Mobile_FPS/Assets/Scripts/PlayerMovementController.cs
+++ Mobile_FPS/Assets/Scripts/PlayerMovementController.cs
@@ -8,12 +8,18 @@ public class PlayerMovementController : MonoBehaviour
     public Joystick joystick;
     public FixedTouchField fixedTouchField;
 
+
+    public float runSpeed = 8f;
+    public float walkSpeed = 5f;
+    
     private RigidbodyFirstPersonController rigidBodyController;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBodyController = GetComponent<RigidbodyFirstPersonController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,5 +33,24 @@ public class PlayerMovementController : MonoBehaviour
         rigidBodyController.joystickInputAxis.x = joystick.Horizontal;
         rigidBodyController.joystickInputAxis.y = joystick.Vertical;
         rigidBodyController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;
+
+        Debug.Log("Horizontal: " + joystick.Horizontal);
+        Debug.Log("Vertical: " + joystick.Vertical);
+
+        animator.SetFloat("Horizontal", joystick.Horizontal);
+        animator.SetFloat("Vertical", joystick.Vertical);
+
+        if (Mathf.Abs(joystick.Horizontal) > 0.9f || Mathf.Abs(joystick.Vertical) > 0.9f)
+        {
+            // Running
+            rigidBodyController.movementSettings.ForwardSpeed = runSpeed;
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            // Not Running
+            rigidBodyController.movementSettings.ForwardSpeed = walkSpeed;
+            animator.SetBool("IsRunning", false);
+        }
     }
 }
